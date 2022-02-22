7ea92144244b5916b1a3cb62f8777d66bc60760a
SeanRosendaal <81298549+SeanRosendaal@users.noreply.github.com>
2022-02-22 12:59:50 +0100

    Category.php
    A catagory from PHP.

---
+++ Herencategorie.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categoriepagina Herenfietsen</title>
+</head>
+<body>
+
+<div class="Container">
+    <header>
+
+    </header>
+    <nav>
+        <ul>
+            <li>
+                <a href="/Categoriepaginas/Categoriepagina%20Damesfietsen/html/Damescategorie.php">
+                    Damesfietsen
+                </a>
+            </li>
+            <li>
+                <a href="/Categoriepaginas/Categoriepagina%20Herenfietsen/html/Herencategorie.php">
+                    Herenfietsen
+                </a>
+            </li>
+            <li>
+                <a href="/Categoriepaginas/Categoriepagina%20Kinderen/html/Kindercategorie.php">
+                    Kinderfietsen
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <div>
+
+    </div>
+    <footer>
+
+    </footer>
+</div>
+
+</body>
+</html>
